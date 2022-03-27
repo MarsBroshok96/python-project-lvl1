04b6bfe1a5ebc45fd41666558107639fcaf95feb
Lokisov.I <marsbroshok1996@gmail.com>
2022-03-27 20:19:53 +0600

    Add script brain-even (step 5)

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+"""Test user's knowledge about numbers parity."""
+from brain_games import parity_game_module
+
+
+def main():
+    """Name as main."""
+    parity_game_module.is_even()
+
+
+if __name__ == '__main__':
+    main()
