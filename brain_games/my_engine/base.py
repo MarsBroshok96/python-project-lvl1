ecd7b2f895a072670642bf2c270c0dfe2000075f
Lokisov.I <marsbroshok1996@gmail.com>
2022-04-03 13:57:49 +0600

    Add brain-gcd game (step 7)

--- brain_games/my_engine/base.py
+++ brain_games/my_engine/base.py
@@ -57,3 +57,26 @@ def generate_task_calc():
         task = '{0} * {1}'.format(first_num, second_num)
         answer = first_num * second_num
     return (task, str(answer))
+
+
+def generate_task_gcd():
+    """
+    Generate new brain_gcd task for player.
+
+    Returns:
+           Task and correct answer.
+    """
+    first_number = random.randint(0, 100)
+    second_number = random.randint(0, 100)
+    task = '{0} {1}'.format(first_number, second_number)
+
+    if first_number < second_number:
+        max_possible_gcd = first_number
+    else:
+        max_possible_gcd = second_number
+
+    for i in range(max_possible_gcd, 0, -1):
+        if first_number % i == 0 and second_number % i == 0:
+            answer = i
+            break
+    return (task, str(answer))
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,11 @@
+"""Test user's GCD search skills."""
+from brain_games.games import brain_gcd
+
+
+def main():
+    """Name as main."""
+    brain_gcd.victory_conditions()
+
+
+if __name__ == '__main__':
+    main()
