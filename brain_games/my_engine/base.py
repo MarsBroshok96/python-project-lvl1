3a9b203862c41e3c75706d9837820085ec1a1e3e
Lokisov.I <marsbroshok1996@gmail.com>
2022-04-03 18:08:54 +0600

    Add brain-progression game (step 8)

--- brain_games/my_engine/base.py
+++ brain_games/my_engine/base.py
@@ -80,3 +80,36 @@ def generate_task_gcd():
             answer = i
             break
     return (task, str(answer))
+
+
+def generate_task_progression():
+    """
+    Generate new brain_progression task for player.
+
+    Returns:
+           Task and correct answer.
+    """
+    def generate_prog():
+        """
+        Generate simple arithmetic progression.
+
+        Returns:
+               start point, end point and step for progression.
+        """
+        start = random.randint(0, 100)
+        step = random.randint(1, 100)
+        length = random.randint(5, 20)   # 5, 20: min and max length
+        end = (start + step * length) + 1
+        return (start, end, step)
+
+    (start, end, step) = generate_prog()
+    progression = list(range(start, end, step))
+    answer = random.choice(progression)
+
+    task = ''
+
+    for i in range(start, end, step):
+        if i == answer:
+            i = '..'
+        task = '{0} {1}'.format(task, str(i))
+    return (task, str(answer))
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+"""Test user's arithmetic and logic skills."""
+from brain_games.games import brain_progression
+
+
+def main():
+    """Name as main."""
+    brain_progression.victory_conditions()
+
+
+if __name__ == '__main__':
+    main()
